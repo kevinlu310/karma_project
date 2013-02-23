a6efe8a31e94f0c1d6b3539908f6ab937c89e87d
Antoine Grondin <antoinegrondin@gmail.com>
2013-02-23 14:35:24 +0400

    Add stub for project model.

--- app/scripts/models/task-model.js
+++ app/scripts/models/task-model.js
@@ -1,8 +1,11 @@
 nyuad.Models.Task = Backbone.Model.extend({
-
    urlRoots: "/api/projects",
    defaults: {
-
+      "title": "Empty title",
+      "description": "Empty description"
+      "picture": "null.png",
+      "funding": 0,
+      "owner": 0
    },
 
    initialize: function () {
