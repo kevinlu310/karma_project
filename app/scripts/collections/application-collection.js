b349f8b1dcae3915b6d66c9965ebd44f2fa9602f
Antoine Grondin <antoinegrondin@gmail.com>
2013-02-23 10:29:20 +0400

    Add init and url methods to Projects collection.

--- app/scripts/collections/application-collection.js
+++ app/scripts/collections/application-collection.js
@@ -5,5 +5,14 @@ nyuad.Collections.ApplicationCollection = Backbone.Collection.extend({
 });
 
 nyuad.Collections.Projects = Backbone.Collection.extend({
-   model: nyuad.Models.Project
+
+   model: nyuad.Models.Project,
+
+   initialize: function (models, options) {
+      this._id = options && options.id ? options.id : null;
+   },
+
+   url: function() {
+      return "/api/projects/" + this._id;
+   }
 });
---
+++ app/scripts/models/project-model.js
@@ -0,0 +1,25 @@
+nyuad.Models.Project = Backbone.Model.extend({
+   urlRoot: "/api/projects",
+   defaults: {
+      "title": "Null Project",
+      "description": "Lorem Ipsumify this",
+      "image_url": "https://upload.wikimedia.org/wikipedia/en/0/0d/Null.png"
+   },
+
+   initialize: function() {
+      // Do something
+      console.log("Hello Project");
+   },
+
+   validate: function() {
+      if(!attr["title"]) {
+         return "Your project must have a title.";
+      }
+      if(!attr["description"]) {
+         return "Your project must have a description.";
+      }
+      if(!attr["image_url"]) {
+         return "Your project must have an image.";
+      }
+   }
+});
