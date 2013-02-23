0e009f13eaba3cec67186a9e253b33ef10e217b9
Antoine Grondin <antoinegrondin@gmail.com>
2013-02-23 18:12:47 +0400

    Stub of ProjectListing. Should display ProjectCards.

--- web/public/js/views/project-listing-view.js
+++ web/public/js/views/project-listing-view.js
@@ -1,3 +1,24 @@
 nyuad.Views.ProjectListing = Backbone.View.extend({
    // Should render ProjectCards onto a grid template
+   template: _.template($("#project-listing-template").html()),
+   events: {
+      // Do stuff
+   },
+   initialize: function() {
+      this.projects = new nyuad.Collections.Projects();
+      this.listenTo(this.projects, "add", this.addOne);
+      this.listenTo(this.projects, "all", this.render);
+      this.projects.fetch();
+   },
+   render: function() {
+      // Do stuff
+   },
+   addOne: function(project) {
+      var project_card = new nyuad.Views.ProjectCard({
+         model: project
+      });
+
+      this.el.append(project_card.render().el);
+
+   }
 });
