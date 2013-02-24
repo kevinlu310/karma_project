4b30ceafc2abbd9203780083a9a38cb29d585877
Antoine Grondin <antoinegrondin@gmail.com>
2013-02-24 11:43:47 +0400

    Update the progress bar on the project cards.

--- web/public/js/models/project-model.js
+++ web/public/js/models/project-model.js
@@ -7,12 +7,45 @@ nyuad.Models.Project = Backbone.Model.extend({
       "picture": "https://upload.wikimedia.org/wikipedia/en/0/0d/Null.png",
       "funding": 0,
       "owner": 0,
-      "funding-percent": 0,
-      "task-percent": 0
+      "funding_percent": 0,
+      "task_percent": 0,
+      "current_fund": 0,
+      "task_count": 0,
+      "task_not_assigned": 0,
+      "user_count": 0
    },
 
    initialize: function() {
+      this.updateTaskPercent();
+      this.updateFundingPercent();
+   },
 
+   updateTaskPercent: function () {
+      console.log("updateTaskPercent");
+      var total = this.get("task_count");
+      var result;
+      if( total ){
+         var assigned = total - this.get("task_not_assigned");
+         result = (assigned/total) * 100;
+      } else {
+         result = 100;
+      }
+      this.set("task_percent",  result);
+      console.log("task percent = " + result);
+   },
+
+   updateFundingPercent: function () {
+      var total = this.get("funding");
+      var result;
+      if( total ){
+         var current = this.get("current_fund");
+         result = (current/total) * 100;
+      } else {
+         result = 100;
+      }
+
+      console.log("fund percent = " + result);
+      this.set("funding_percent", result);
    },
 
    validate: function() {
@@ -24,9 +57,6 @@ nyuad.Models.Project = Backbone.Model.extend({
       }
       if(!attr["image_url"]) {
          return "Your project must have an image.";
-      }
-      if(!attr["Current-Fund"]){
-         console.log("Current-Fund is null");
       }
    }
 });
