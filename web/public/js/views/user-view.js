a5ad3675a2b21dea36b3be7af38c97c8de52cf94
KalthoomAlAwar <kalthoom.alawar@gmail.com>
2013-02-24 13:09:52 +0400

    user tasks list in user page

--- web/public/js/views/user-view.js
+++ web/public/js/views/user-view.js
@@ -3,6 +3,7 @@ nyuad.Views.UserView = Backbone.View.extend({
    el: "#main-content",
    template: _.template($("#user-view-template").html()),
    fundedTemplate: _.template($("#funds-item-template").html()),
+   tasksTemplate: _.template($("#user-tasks-list-template").html()),
 
    events: {
       "click .btn": "onClick"
@@ -18,10 +19,14 @@ nyuad.Views.UserView = Backbone.View.extend({
       this.$el.html(this.template(this.model.toJSON()));
 
       var fundedProjects = this.model.get("funded_projects") || [];
+      var tasksProjects = this.model.get("tasks_projects") || [];
       var that = this;
       _.each(fundedProjects, function(project) {
          this.$("#funds-list").append(that.fundedTemplate(project));
-      })
+      });
+      _.each(tasksProjects, function(task) {
+         this.$("#user-tasks-list").append(that.tasksTemplate(task));
+      });
       return this;
    },
 
